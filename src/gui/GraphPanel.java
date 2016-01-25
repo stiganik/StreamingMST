f92c40afa37ae1fe8d07276f349fa6ed0735f867
Sander Tiganik <sander.tiganik@gmail.com>
2016-01-25 14:01:23 +0200

    Create GraphPanel. Add pop up window to GUI.

---
+++ src/gui/GraphPanel.java
@@ -0,0 +1,16 @@
+package gui;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GraphPanel extends JPanel {
+	public GraphPanel() {
+		
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+	}
+}
