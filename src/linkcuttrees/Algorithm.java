c09706a867e8c985344d4358d3a009d80ff31469
Jaan Janno <jaan911@ut.ee>
2016-01-25 22:56:02 +0200

    Edit algorithm.

--- src/linkcuttrees/Algorithm.java
+++ src/linkcuttrees/Algorithm.java
@@ -3,6 +3,7 @@ package linkcuttrees;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -18,12 +19,24 @@ public class Algorithm implements StreamingMST {
 	LinkCutTree tree = new LinkCutTreeNaive();
 	
 	@Override
+	public ArrayList<Edge> getEdges() {
+		ArrayList<Edge> edgeList = new ArrayList<>();
+		for(Vertex v: edges.keySet()) {
+			Map<Vertex, Integer> dest = edges.get(v);
+			for(Vertex v2: dest.keySet()) {
+				edgeList.add(new Edge(v, v2, dest.get(v2)));
+			}
+		}
+		return edgeList;
+	}
+	
+	@Override
 	public void newEdge(Edge edge) {
 		Vertex v1 = edge.getVertex1();
 		Vertex v2 = edge.getVertex2();
 		int weight = edge.getWeight();
 		
-		if(!vertices.contains(v1) && ! vertices.contains(v2)) {
+		if(!vertices.contains(v1) && !vertices.contains(v2)) {
 			handleNewEdges(v1, v2, weight);			
 		}
 		else if(vertices.contains(v1)) {
@@ -39,8 +52,91 @@ public class Algorithm implements StreamingMST {
 	}
 	
 	private void handleOldEdges(Vertex v1, Vertex v2, int weight) {
-		// TODO Auto-generated method stub
+		if(tree.findRoot(v1) != tree.findRoot(v2)) {
+			mergeTrees(v1, v2, weight);
+		}
+		else {
+			breakCycle(v1, v2, weight);
+		}
+	}
+	
+	private void breakCycle(Vertex v1, Vertex v2, int weight) {
+		List<Vertex> path1 = tree.findPath(v1);
+		List<Vertex> path2 = tree.findPath(v2);	
+		unifyPaths(path1, path2);
 		
+		int currentMaxWeight = weight;
+		Vertex currentMaxVert1 = v1;
+		Vertex currentMaxVert2 = v2;
+		
+		for(int i = 0; i < path1.size()-1; i++) {
+			Vertex measuredVert1 = path1.get(i);
+			Vertex measuredVert2 = path1.get(i+1);
+			int measuredWeight = measureEdge(measuredVert1, measuredVert2);
+			
+			if(measuredWeight > currentMaxWeight) {
+				currentMaxWeight = measuredWeight;
+				currentMaxVert1 = measuredVert1;
+				currentMaxVert2 = measuredVert2;
+			}
+		}
+		
+		for(int i = 0; i < path2.size()-1; i++) {
+			Vertex measuredVert1 = path1.get(i);
+			Vertex measuredVert2 = path1.get(i+1);
+			int measuredWeight = measureEdge(measuredVert1, measuredVert2);
+			
+			if(measuredWeight > currentMaxWeight) {
+				currentMaxWeight = measuredWeight;
+				currentMaxVert1 = measuredVert1;
+				currentMaxVert2 = measuredVert2;
+			}
+		}
+		
+		if(currentMaxWeight == weight) {
+			return;
+		}
+		else {
+			rememberEdge(v1, v2, weight);
+			forgetEdge(currentMaxVert1, currentMaxVert2);
+			
+			tree.cut(currentMaxVert1);
+			rotate(v1);
+			rotate(v2);
+			tree.link(v1, v2);
+		}
+		
+	}
+
+	private void unifyPaths(List<Vertex> path1, List<Vertex> path2) {
+		Vertex previous = null;
+		while(path1.get(0) == path2.get(0)) {
+			previous = path1.get(0);
+			path1.remove(0);
+			path2.remove(0);
+		}
+		path1.add(0, previous);
+		path2.add(0, previous);
+	}
+
+	private void mergeTrees(Vertex v1, Vertex v2, int weight) {
+		rememberEdge(v1, v2, weight);
+		
+		rotate(v1);
+		rotate(v2);
+		tree.link(v1, v2);
+	}
+
+	private void rotate(Vertex v) {
+		List<Vertex> path = tree.findPath(v);
+		
+		for(int i = path.size() - 1; i > 0; i--) {
+			Vertex parent = path.get(i);
+			Vertex child = path.get(i-1);
+			
+			tree.cut(child);
+			tree.link(parent, child);
+		}
 	}
 
 	private void handleNewEdge(Vertex old, Vertex newVertex, int weight) {
@@ -66,16 +162,13 @@ public class Algorithm implements StreamingMST {
 		tree.link(v1, v2);
 	}
 	
-	@Override
-	public ArrayList<Edge> getEdges() {
-		ArrayList<Edge> edgeList = new ArrayList<>();
-		for(Vertex v: edges.keySet()) {
-			Map<Vertex, Integer> dest = edges.get(v);
-			for(Vertex v2: dest.keySet()) {
-				edgeList.add(new Edge(v, v2, dest.get(v2)));
-			}
+	private int measureEdge(Vertex v1, Vertex v2) {
+		if(v1.toString().compareTo(v2.toString()) < 0) {
+			return edges.get(v1).get(v2);
 		}
-		return edgeList;
+		else {
+			return edges.get(v2).get(v1);
+		}
 	}
 	
 	private void forgetEdge(Vertex v1, Vertex v2) {
