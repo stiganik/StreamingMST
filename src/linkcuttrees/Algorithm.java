c87542ab2e2fdb0f7ef7863fa7d8139e4dd604f4
Jaan Janno <jaan911@ut.ee>
2016-01-25 13:53:58 +0200

    Add definition for edge.

--- src/linkcuttrees/Algorithm.java
+++ src/linkcuttrees/Algorithm.java
@@ -1,6 +1,29 @@
 package linkcuttrees;
 
-public class Algorithm {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import algorithm.StreamingMST;
+import dataTypes.Edge;
+
+public class Algorithm implements StreamingMST {
+	
+	Set<Vertex> vertices = new HashSet<>();
+	Map<Vertex, Map<Vertex, Integer>> edges = new HashMap<>();
+	
+	@Override
+	public void newEdge(Edge edge) {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public ArrayList<Edge> getEdges() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	
 
