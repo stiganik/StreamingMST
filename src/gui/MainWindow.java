83467e1b4d27996bfafed4d89b3acc32a326d095
Sander Tiganik <sander.tiganik@gmail.com>
2016-01-25 13:38:23 +0200

    Fix unequal space distribution, simplify GUI.

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -2,7 +2,6 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -34,40 +33,43 @@ public class MainWindow {
 		JMenuBar menuBar = createMenuBar();
 		frame.setJMenuBar(menuBar);
 		
-		JToolBar editToolBar = createEditToolBar();
-		JToolBar resultToolBar = createResultToolBar();
-		JPanel edit = new JPanel(new GridBagLayout());
-		JPanel result = new JPanel(new GridBagLayout());
+		JToolBar toolBar = createToolBar();
+		
+		JPanel edit = new JPanel();
+		edit.setBackground(new Color(200, 200, 200));
+		JPanel result = new JPanel();
+		result.setBackground(new Color(255, 255, 255));
 		
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.BOTH;
-		c.weightx = 0.5;
+		
 		c.gridx = 0;
 		c.gridy = 0;
-		
+		c.gridwidth = 2;
+		c.weightx = 1;
 		c.weighty = 0.02;
-		content.add(editToolBar,c);
-		c.gridx += 1;
-		content.add(resultToolBar, c);
-		c.gridx -= 1;
-		c.gridy += 1;
-		c.weighty = 1 - c.weighty;
-		edit.setBackground(new Color(200, 200, 200));
-		content.add(edit,c);
-		c.gridx += 1;
-		result.setBackground(new Color(255, 255, 255));
-		content.add(result,c);
+		
+		content.add(toolBar, c);
+		
+		c.gridwidth = 1;
+		c.weightx = 0.5;
+		c.gridy = 1;
+		c.weighty = 0.98;
+		
+		content.add(edit, c);
+		c.gridx = 1;
+		content.add(result, c);
 		
 		frame.setResizable(false);
 		frame.setVisible(true);
 	}
 
-	private JToolBar createResultToolBar() {
+	private JToolBar createToolBar() {
 		JToolBar toolBar = new JToolBar();
 		
 		toolBar.addSeparator();
 		
-		JButton button = new JButton("Restart");
+		JButton button = new JButton("Reset");
 		button.setFocusPainted(false);
 		button.addActionListener(new ActionListener() {
 			@Override
@@ -80,37 +82,6 @@ public class MainWindow {
 		toolBar.addSeparator();
 		
 		button = new JButton("Start");
-		button.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
-		button.setFocusPainted(false);
-		toolBar.add(button);
-		
-		toolBar.setFloatable(false);
-		return toolBar;
-	}
-
-	private JToolBar createEditToolBar() {
-		JToolBar toolBar = new JToolBar();
-		
-		toolBar.addSeparator();
-		
-		JButton button = new JButton("Add Edge");
-		button.setFocusPainted(false);
-		button.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
-		toolBar.add(button);
-		
-		toolBar.addSeparator();
-		
-		button = new JButton("Move Node");
 		button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
